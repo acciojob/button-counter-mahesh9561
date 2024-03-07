66484e471233b3816461eba14eeb70653f1401f1
mahesh9561 <maheshpathak200@gmail.com>
2024-03-07 18:06:45 +0530

    new-changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [state, setState] = useState(0);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setState(state + 1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h4>Button clicked {state} times</h4>
+      <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
